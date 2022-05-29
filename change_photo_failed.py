8ece7e2ef92b7a199a7bfd9c1a310a13fb1ee3e0
bengisuucolak <49275442+bengisuucolak@users.noreply.github.com>
2022-05-29 04:58:37 +0300

    Update change_photo_failed.py

--- change_photo_failed.py
+++ change_photo_failed.py
@@ -18,8 +18,8 @@ except:
 email = driver.find_element_by_id("email")
 password = driver.find_element_by_id("password")
 
-email.send_keys("andromeda.2022.test@gmail.com")
-password.send_keys("andromeda123")
+email.send_keys("*******.test@gmail.com")
+password.send_keys("a**********123")
 
 try:
     profile = driver.find_element_by_xpath('/html/body/header/div/ol[2]/li[2]/a/div[1]/img')
