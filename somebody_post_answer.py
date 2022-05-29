80b7f10ed45880aae77425ad983764ca88175b9b
bengisuucolak <49275442+bengisuucolak@users.noreply.github.com>
2022-05-29 05:01:34 +0300

    Update somebody_post_answer.py

--- somebody_post_answer.py
+++ somebody_post_answer.py
@@ -17,8 +17,8 @@ except:
 email = driver.find_element_by_id("email")
 password = driver.find_element_by_id("password")
 
-email.send_keys("andromeda.2022.test@gmail.com")
-password.send_keys("andromeda123")
+email.send_keys("*******.test@gmail.com")
+password.send_keys("a*********123")
 
 try:
     question_s = driver.find_element_by_xpath('//*[@id="nav-questions"]/span')
